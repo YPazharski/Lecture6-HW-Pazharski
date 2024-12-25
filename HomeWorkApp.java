7d4b92e3ab1daa70f0bd67222faaa561618b7a7f
Yury Pazharski <Pozharskiyuri@gmail.com>
2024-12-25 13:27:25 +0300

    Add printColor()

--- HomeWorkApp.java
+++ HomeWorkApp.java
@@ -1,6 +1,8 @@
 public class HomeWorkApp {
     public static void main(String[] args) {
+        printThreeWords();
         checkSumSign();
+        printColor();
     }
 
     public static void printThreeWords() {
@@ -10,5 +12,17 @@ public class HomeWorkApp {
     public static void checkSumSign() {
         int a = Integer.MIN_VALUE, b = Integer.MAX_VALUE;
         System.out.println(a + b >= 0 ? "Сумма положительная" : "Сумма отрицательная");
+    }
+
+    public static void printColor() {
+        int value = 888;
+        if (value <= 0) {
+            System.out.println("Красный"); }
+        else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        else {
+            System.out.println("Зеленый");
+        }
     }
 }
