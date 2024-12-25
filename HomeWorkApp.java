8d6e899c5d14bb1da1e82a9367a25d7148e17f08
Yury Pazharski <Pozharskiyuri@gmail.com>
2024-12-25 13:12:10 +0300

    Add checkSumSign()

--- HomeWorkApp.java
+++ HomeWorkApp.java
@@ -1,9 +1,14 @@
 public class HomeWorkApp {
     public static void main(String[] args) {
-
+        checkSumSign();
     }
 
     public static void printThreeWords() {
         System.out.println("Orange\nBanana\nApple");
+    }
+
+    public static void checkSumSign() {
+        int a = Integer.MIN_VALUE, b = Integer.MAX_VALUE;
+        System.out.println(a + b >= 0 ? "Сумма положительная" : "Сумма отрицательная");
     }
 }
