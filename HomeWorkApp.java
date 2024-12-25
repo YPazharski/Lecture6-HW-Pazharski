ea9977027a2a9574cf149c41485a3a9f2d4f0828
Yury Pazharski <Pozharskiyuri@gmail.com>
2024-12-25 13:02:06 +0300

    Add printThreeWords()

--- HomeWorkApp.java
+++ HomeWorkApp.java
@@ -2,4 +2,8 @@ public class HomeWorkApp {
     public static void main(String[] args) {
 
     }
+
+    public static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
 }
