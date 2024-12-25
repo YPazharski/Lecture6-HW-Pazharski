2c399eebe5196e6ce1698b6bf80fceda024db68f
Yury Pazharski <Pozharskiyuri@gmail.com>
2024-12-25 13:43:19 +0300

    Optimization of printColor() =)

--- HomeWorkApp.java
+++ HomeWorkApp.java
@@ -19,7 +19,7 @@ public class HomeWorkApp {
         int value = 888;
         if (value <= 0) {
             System.out.println("Красный"); }
-        else if (value > 0 && value <= 100) {
+        else if (value <= 100 && value > 0) {
             System.out.println("Желтый");
         }
         else {
